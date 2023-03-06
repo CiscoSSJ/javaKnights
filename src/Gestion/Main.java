2bc3ba34d023765e2af300c7e484b27e157538ce
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2023-03-07 00:02:18 +0100

    Arreglo de menú dificultad
    Se hace funcional la y consigue que se deshabilite y habilite uno de los botones.

--- src/Gestion/Main.java
+++ src/Gestion/Main.java
@@ -19,7 +19,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Main extends JFrame implements ActionListener {
+class Main extends JFrame implements ActionListener {
 
     JButton botonIniciar;
     JLabel etiquetaTitulo;
@@ -32,48 +32,34 @@ public class Main extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // Crear componentes
         etiquetaTitulo = new JLabel("Bienvenido al juego");
         botonIniciar = new JButton("Iniciar");
-
-        // Configurar el botón de inicio
         botonIniciar.addActionListener(this);
 
-        // Crear un panel y agregar componentes
         panel = new JPanel(new BorderLayout());
         panel.add(etiquetaTitulo, BorderLayout.CENTER);
         panel.add(botonIniciar, BorderLayout.SOUTH);
 
-        // Agregar el panel a la ventana
         getContentPane().add(panel);
-
-        // Mostrar la ventana
-        setVisible(true);
-        
+        setVisible(true);  
     }
 
     public static void main(String[] args) {
         new Main();
-        // Mensaje de Inicio de juego
         System.out.println("Iniciando juego...");
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == botonIniciar) {
-            // Acción al presionar el botón de inicio
-            dispose(); // Cerrar la ventana actual
-            MenuPrincipal menuPrincipal = new MenuPrincipal(); // Crear un nuevo menú
-            menuPrincipal.setVisible(true); // Mostrar el nuevo menú
+            dispose(); 
+            new MenuPrincipal().setVisible(true);
         }
     }
-
 }
 
 class MenuPrincipal extends JFrame implements ActionListener {
 
-    JButton botonIniciarPartida;
-    JButton botonIniciarTutorial;
-    JButton botonConfiguracion;
+    JButton botonIniciarPartida, botonIniciarTutorial, botonConfiguracion;
     JLabel etiquetaTitulo;
     JPanel panel;
 
@@ -90,224 +76,145 @@ class MenuPrincipal extends JFrame implements ActionListener {
         botonIniciarTutorial = new JButton("Iniciar tutorial");
         botonConfiguracion = new JButton("Configuración");
 
-        // Configurar los botones
         botonIniciarPartida.addActionListener(this);
         botonIniciarTutorial.addActionListener(this);
         botonConfiguracion.addActionListener(this);
 
-        // Crear un panel y agregar componentes
         panel = new JPanel(new GridLayout(4, 1));
         panel.add(etiquetaTitulo);
         panel.add(botonIniciarPartida);
         panel.add(botonIniciarTutorial);
         panel.add(botonConfiguracion);
 
-        // Agregar el panel a la ventana
         getContentPane().add(panel);
-
-        // Mostrar la ventana
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
+        dispose();
         if (e.getSource() == botonIniciarPartida) {
-            // Acción al presionar el botón de iniciar partida
-            System.out.println("Iniciando partida...");
-            dispose(); // Cerrar la ventana actual
-            MenuIniciarPartida MenuIniciarPartida = new MenuIniciarPartida(); // Crear un nuevo menú
-            MenuIniciarPartida.setVisible(true); // Mostrar el nuevo menú
-            
+            new MenuIniciarPartida().setVisible(true);
         } else if (e.getSource() == botonIniciarTutorial) {
-            // Acción al presionar el botón de iniciar tutorial
             System.out.println("Iniciando tutorial...");
-            dispose(); // Cerrar la ventana actual
         } else if (e.getSource() == botonConfiguracion) {
-            // Acción al presionar el botón de configuración
             System.out.println("Abriendo configuración...");
-            dispose(); // Cerrar la ventana actual
         }
     }
-
 }
 
 class MenuIniciarPartida extends JFrame implements ActionListener {
 	
-	JButton botonNuevaPartida;
-    JButton botonCargarPartida;
+	JButton botonNuevaPartida, botonCargarPartida;
     JLabel etiquetaTitulo;
     JPanel panel;
 
     public MenuIniciarPartida() {
-        // Configurar la ventana
         setTitle("Menú iniciar partida");
         setSize(400, 300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // Crear componentes
         etiquetaTitulo = new JLabel("Menú inicio de partida");
         botonNuevaPartida = new JButton("Nueva partida");
         botonCargarPartida = new JButton("Cargar partida");
 
-        // Configurar los botones
         botonNuevaPartida.addActionListener(this);
         botonCargarPartida.addActionListener(this);
 
-        // Crear un panel y agregar componentes
         panel = new JPanel(new GridLayout(3, 1));
         panel.add(etiquetaTitulo);
         panel.add(botonNuevaPartida);
         panel.add(botonCargarPartida);
 
-        // Agregar el panel a la ventana
         getContentPane().add(panel);
-
-        // Mostrar la ventana
         setVisible(true);
     }
 	
     public void actionPerformed(ActionEvent e) {
+        dispose();
         if (e.getSource() == botonNuevaPartida) {
-            // Acción al presionar el botón de iniciar partida
-            System.out.println("Iniciando partida...");
-            MenuNuevaPartida MenuNuevaPartida = new MenuNuevaPartida(); // Crear un nuevo menú
-            MenuNuevaPartida.setVisible(true); // Mostrar el nuevo menú
-            dispose(); // Cerrar la ventana actual
-            
+            new MenuNuevaPartida().setVisible(true);
         } else if (e.getSource() == botonCargarPartida) {
-            // Acción al presionar el botón de cargar partida
             System.out.println("Cargando partida...");
-            dispose(); // Cerrar la ventana actual
         }
     }
 }
 
 class MenuNuevaPartida extends JFrame implements ActionListener {
 	
-	private static boolean dificultadSeleccionada = false;
-	JButton botonDificultad;
-    JButton botonIniciarTutorial;
-    JButton botonIniciarCreador;
-    JLabel etiquetaTitulo;
-    JPanel panel;
+	JButton facil, medio, dificil;
+    JButton iniciar;
+    String dificultadSeleccionada;
 
     public MenuNuevaPartida() {
-        // Configurar la ventana
+    	
+    	// Configurar la ventana
         setTitle("Menú nueva partida");
         setSize(400, 300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        
         // Crear componentes
-        etiquetaTitulo = new JLabel("Menú nueva partida");
-        botonDificultad = new JButton("Dificultad");
-        botonIniciarTutorial = new JButton("Iniciar partida");
-        botonIniciarCreador = new JButton("Iniciar creador");
+        Container contenedor = getContentPane();
+        contenedor.setLayout(new GridLayout(3, 1));
+
+        JLabel mensaje = new JLabel("Seleccione la dificultad");
+        mensaje.setHorizontalAlignment(JLabel.CENTER);
+        contenedor.add(mensaje);
+
+        JPanel panelBotones = new JPanel(new GridLayout(1, 3));
+        facil = new JButton("Fácil");
+        medio = new JButton("Medio");
+        dificil = new JButton("Difícil");
+        panelBotones.add(facil);
+        panelBotones.add(medio);
+        panelBotones.add(dificil);
+        contenedor.add(panelBotones);
+
+        iniciar = new JButton("Iniciar partida");
+        iniciar.setEnabled(false);
+        contenedor.add(iniciar);
 
         // Configurar los botones
-        botonDificultad.addActionListener(this);
-		botonIniciarTutorial.addActionListener(this);
-		if (!dificultadSeleccionada) {
-			botonIniciarCreador.setEnabled(false); // deshabilitar el botón
-        } else {
-        	botonIniciarCreador.addActionListener(this); // dejarlo habilitado
-        }
+        facil.addActionListener(this);
+        medio.addActionListener(this);
+        dificil.addActionListener(this);
 
-        // Crear un panel y agregar componentes
-        panel = new JPanel(new GridLayout(4, 1));
-        panel.add(etiquetaTitulo);
-        panel.add(botonDificultad);
-        panel.add(botonIniciarTutorial);
-        panel.add(botonIniciarCreador);
-
-        // Agregar el panel a la ventana
-        getContentPane().add(panel);
+        iniciar.addActionListener(this);
 
         // Mostrar la ventana
         setVisible(true);
     }
 	
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == botonDificultad) {
-            // Acción al presionar el botón de elegir dificultad
-            System.out.println("Iniciando seleccion de dificultad...");
-            dispose(); // Cerrar la ventana actual
-            MenuSeleccionDificultad menuSeleccionDificultad = new MenuSeleccionDificultad(); // Crear un nuevo menú
-            menuSeleccionDificultad.setVisible(true); // Mostrar el nuevo menú
-            
-        } else if (e.getSource() == botonIniciarTutorial) {
-            // Acción al presionar el botón de iniciar tutorial
-            System.out.println("Iniciando tutorial...");
-            dispose(); // Cerrar la ventana actual
-            
-        } else if (e.getSource() == botonIniciarCreador) {
-            // Acción al presionar el botón de iniciar creador
-            System.out.println("Iniciando creador de personaje...");
-            dispose(); // Cerrar la ventana actual
+    	if (e.getSource() == facil) {
+    		// Acción al presionar el botón de dificultad fácil
+            iniciar.setEnabled(true);
+            medio.setEnabled(false);
+            dificil.setEnabled(false);
+            dificultadSeleccionada = "Fácil";
         }
-    }
-}
 
-class MenuSeleccionDificultad extends JFrame implements ActionListener {
-	
-	JButton botonSeleccionPrincipiante;
-    JButton botonSeleccionAficionado;
-    JButton botonSeleccionAvanzado;
-    JLabel etiquetaTitulo;
-    JPanel panel;
+        if (e.getSource() == medio) {
+        	// Acción al presionar el botón de dificultad medio
+            iniciar.setEnabled(true);
+            facil.setEnabled(false);
+            dificil.setEnabled(false);
+            dificultadSeleccionada = "Medio";
+        }
 
-    public MenuSeleccionDificultad() {
-        // Configurar la ventana
-        setTitle("Menú para la seleccion de dificultad");
-        setSize(400, 300);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        if (e.getSource() == dificil) {
+        	// Acción al presionar el botón de dificultad difícil
+            iniciar.setEnabled(true);
+            facil.setEnabled(false);
+            medio.setEnabled(false);
+            dificultadSeleccionada = "Difícil";
+        }
 
-        // Crear componentes
-        etiquetaTitulo = new JLabel("Menú nueva partida");
-        botonSeleccionPrincipiante = new JButton("Principiante");
-        botonSeleccionAficionado = new JButton("Aficionado");
-        botonSeleccionAvanzado = new JButton("Avanzado");
-
-        // Configurar los botones
-        botonSeleccionPrincipiante.addActionListener(this);
-        botonSeleccionAficionado.addActionListener(this);
-        botonSeleccionAvanzado.addActionListener(this);
-
-        // Crear un panel y agregar componentes
-        panel = new JPanel(new GridLayout(4, 1));
-        panel.add(etiquetaTitulo);
-        panel.add(botonSeleccionPrincipiante);
-        panel.add(botonSeleccionAficionado);
-        panel.add(botonSeleccionAvanzado);
-
-        // Agregar el panel a la ventana
-        getContentPane().add(panel);
-
-        // Mostrar la ventana
-        setVisible(true);
-    }
-	
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == botonSeleccionPrincipiante) {
-            // Acción al presionar el botón de elegir dificultad principiante
-            System.out.println("Dificultad principiante seleccionada");
-            dispose(); // Cerrar la ventana actual
-            MenuNuevaPartida MenuNuevaPartida = new MenuNuevaPartida(); // Crear un nuevo menú
-            MenuNuevaPartida.setVisible(true); // Mostrar el nuevo menú
-            
-            
-        } else if (e.getSource() == botonSeleccionAficionado) {
-            // Acción al presionar el botón de elegir dificultad aficionado
-            System.out.println("Dificultad aficionado seleccionada");
-            dispose(); // Cerrar la ventana actual
-            MenuNuevaPartida MenuNuevaPartida = new MenuNuevaPartida(); // Crear un nuevo menú
-            MenuNuevaPartida.setVisible(true); // Mostrar el nuevo menú
-            
-        } else if (e.getSource() == botonSeleccionAvanzado) {
-            // Acción al presionar el botón de elegir dificultad avanzado
-            System.out.println("Dificultad avanzado seleccionada");
-            dispose(); // Cerrar la ventana actual
+        if (e.getSource() == iniciar) {
+            JOptionPane.showMessageDialog(null, "Iniciando partida en modo " + dificultadSeleccionada + "...");
+            dispose(); // cierra la ventana principal
+            JOptionPane.getRootFrame().dispose(); // cierra el mensaje de diálogo
         }
     }
 }
