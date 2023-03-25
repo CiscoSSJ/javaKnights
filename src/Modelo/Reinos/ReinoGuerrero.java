e0a229247dde4e8602cc728266a4d782334133fc
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2023-03-25 13:31:39 +0100

    Se añaden comentarios
    Se añaden comentarios a rama Jesus.

--- src/Modelo/Reinos/ReinoGuerrero.java
+++ src/Modelo/Reinos/ReinoGuerrero.java
@@ -2,14 +2,25 @@ package Modelo.Reinos;
 
 import Modelo.Personaje.Personaje;
 
+/**
+ * La siguiente clase Java extiende la clase Reino y representa un Reino Guerrero.
+ */
 public class ReinoGuerrero extends Reino {
 
+	/**
+	 * Constructor de la clase ReinoGuerrero que llama
+	 * al constructor de la clase padre Reino.
+	 */
 	public ReinoGuerrero() {
 		super("Alejandro Magno", new String[] { "Jon Nieve", "Jaime Lannister", "Brienne de Tarth" }, "Reino Guerrero",
 				3);
 
 	}
 
+	/**
+	 * Sobrescribe el método debilidad de la clase padre Reino.
+	 * Si comprobar es verdadero, el personaje defiende.
+	 */
 	@Override
 	public void debilidad(Personaje personaje, boolean comprobar) {
 		if (comprobar)
