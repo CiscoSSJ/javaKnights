9241bd1fcf29a0b59907cb3434970ef317e184de
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2023-03-25 13:32:15 +0100

    Se añaden comentarios
    Se añaden comentarios a rama Jesus.

--- src/Modelo/Reinos/ReinoHielo.java
+++ src/Modelo/Reinos/ReinoHielo.java
@@ -2,16 +2,28 @@ package Modelo.Reinos;
 
 import Modelo.Personaje.Personaje;
 
+/**
+ * Esta clase se llama ReinoHielo y extiende la clase abstracta Reino.
+ * Representa un reino de hielo en un mundo ficticio.
+ */
 public class ReinoHielo extends Reino {
-
+	
+	/**
+	 *Llama al constructor de la clase Reino pasando el nombre del rey,
+	 *un arreglo de nombres de personajes importantes en el reino,
+	 * el nombre del reino y el nivel.
+	 */
 	public ReinoHielo() {
 		super("Rey de la noche", new String[] { "Elsa de Frozen", "Caminante Blanco", "Gared Tollett" }, "Reino Hielo",
 				4);
 
 	}
 
+	/**
+	 * Un método que representa la debilidad del reino de hielo.
+	 * Si comprobar es verdadero, entonces el personaje pierde resistencia al desgaste.
+	 */
 	@Override
-
 	public void debilidad(Personaje personaje, boolean comprobar) {
 		if (comprobar)
 			personaje.desgastar();
