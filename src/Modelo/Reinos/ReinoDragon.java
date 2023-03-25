bb7e4209a0fafaca3a967f526a404f88e7c3fbc2
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2023-03-25 13:30:45 +0100

    Se añaden comentarios
    Se añaden comentarios a rama Jesus

--- src/Modelo/Reinos/ReinoDragon.java
+++ src/Modelo/Reinos/ReinoDragon.java
@@ -2,22 +2,34 @@ package Modelo.Reinos;
 
 import Modelo.Personaje.Personaje;
 
-
+/**
+ * Esta es la clase "ReinoDragon" que extiende la clase abstracta "Reino".
+ * La clase define el constructor "ReinoDragon" y el método "debilidad" que
+ * anula el método abstracto de la clase padre "Reino".
+ */
 public class ReinoDragon extends Reino {
-	
 
+	/**
+	 * Constructor de la clase "ReinoDragon" que llama al constructor de la clase
+	 * padre "Reino" con los siguientes parámetros: "Veighar" (nombre del rey),
+	 * ["Droghon", "Raeghal", "Viserion"] (nombres de los dragones), 
+	 * "Reino Dragon" (nombre del reino), 1 (nivel del reino).
+	 */
 	public ReinoDragon() {
 		super("Veighar", new String[] { "Droghon", "Raeghal", "Viserion" }, "Reino Dragon", 1);
 
 	}
 
+	/**
+	 * Este método anula el método abstracto "debilidad" de la clase padre "Reino".
+	 * Si el parámetro "comprobar" es verdadero, el método invoca al método
+	 * "hablar" del objeto "personaje" pasado como parámetro.
+	 */
 	@Override
-	public void debilidad(Personaje personaje,boolean comprobar) {
-		if(comprobar)
+	public void debilidad(Personaje personaje, boolean comprobar) {
+		if (comprobar)
 			personaje.hablar();
-			
 
 	}
-
 
 }
