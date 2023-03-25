6c601815f0469e724268ec66586595c17a536533
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2023-03-25 13:23:13 +0100

    Se añade comentario.
    Se añade comentario a rama de Jesus.

--- src/Gestion/Main.java
+++ src/Gestion/Main.java
@@ -5,8 +5,12 @@ import Modelo.Partida.Partida;
 public class Main {
 
 	public static void main(String[] args) {
-		//Inica la partida
+		// Crea una nueva instancia de Partida
 		Partida partida1 = new Partida();
+		/**
+		 * Llama al método menuPrincipal() de la clase Menus de la
+		 * instancia de Partida creada anteriormente.
+		 */
 		partida1.getMenus().menuPrincipal(partida1);
 	}
 }
