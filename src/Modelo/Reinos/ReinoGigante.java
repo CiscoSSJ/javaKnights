50543917cb8d1fdc393e51cfcef6191cd04838f1
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2023-03-25 13:31:16 +0100

    Se añaden comentarios
    Se añaden comentarios a rama Jesus.

--- src/Modelo/Reinos/ReinoGigante.java
+++ src/Modelo/Reinos/ReinoGigante.java
@@ -2,14 +2,24 @@ package Modelo.Reinos;
 
 import Modelo.Personaje.Personaje;
 
+/**
+ * Clase que representa el Reino de los Gigantes en el modelo.
+ * La clase ReinoGigante hereda de la clase Reino.
+ */
 public class ReinoGigante extends Reino {
 
+	/**
+	 * Constructor de la clase ReinoGigante que inicializa los atributos específicos del reino gigante
+	 */
 	public ReinoGigante() {
 		super("Gigante de hierro", new String[] { "Mag el Poderoso", "Wun Weg Wun Dar Wun", "Dongo el gigante" },
 				"Reino Gigante", 2);
 
 	}
 
+	/**
+	 * Implementación del método debilidad de la clase Reino que permite a un personaje esquivar.
+	 */
 	@Override
 	public void debilidad(Personaje personaje,boolean comprobar) {
 		if(comprobar)
