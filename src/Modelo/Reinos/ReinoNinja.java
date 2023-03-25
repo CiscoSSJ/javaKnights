0661bcd30bbbb4e119247345c853ba6612f46999
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2023-03-25 13:32:46 +0100

    Se añaden comentarios
    Se añaden comentarios a rama Jesus.

--- src/Modelo/Reinos/ReinoNinja.java
+++ src/Modelo/Reinos/ReinoNinja.java
@@ -2,12 +2,21 @@ package Modelo.Reinos;
 
 import Modelo.Personaje.Personaje;
 
+
 public class ReinoNinja extends Reino {
 
+	/**
+	 * Constructor para la clase ReinoNinja que llama al constructor de la superclase Reino.
+	 */
 	public ReinoNinja() {
 		super("Bruce Lee", new String[] { "Hattori Hanzo", "Goemon Ishikawa", "Saizo Kirigakure" }, "Reino Ninja", 5);
 	}
 
+	/**
+	 * Método que establece la debilidad de un personaje de acuerdo a un booleano comprobar.
+	 * Si el booleano es verdadero, 
+	 * se llama al método silencio() del objeto Personaje recibido como parámetro.
+	 */
 	@Override
 	public void debilidad(Personaje personaje, boolean comprobar) {
 		if (comprobar)
